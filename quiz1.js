e9ca1f183715891122764537fde1aa0d40a7a67b
jellyeast <80386870+jellyeast@users.noreply.github.com>
2021-03-24 16:47:39 +0800

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,6 @@
+function change() {     
+		var header = document.getElementById("H1");     
+		header.innerHTML = "CSIE@CGU";
+		var para = document.getElementById("P");
+		para.innerHTML = "怎麼那麼棒！！.";   
+		}
